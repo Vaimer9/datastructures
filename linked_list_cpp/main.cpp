c3ca7acdee6c1e02e0687097afc54f528953c5d2
Vaimer9 <vaim3r9@gmail.com>
2022-03-20 17:27:53 +0530

    Small changes

--- linked_list_cpp/main.cpp
+++ linked_list_cpp/main.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <iostream>
 
+#define NEWNODE (Node<T>*) malloc(sizeof (Node<T>));
+
 using namespace std;
 
 template <typename T>
@@ -13,79 +15,90 @@ struct Node {
 
 template <typename T>
 class LinkedList {
+
+    private:
+
+    void clear_elements()
+    {
+        Node<T> *tmp;
+
+        while (head)
+        {
+            tmp = head;
+            head = head->next;
+            free(tmp);
+        }
+    }
     public:
 
-        Node<T> *head;
+    Node<T> *head;
 
-        LinkedList()
-        {
-            Node<T> *tmp = (Node<T>*) malloc(sizeof (Node<T>));
+    LinkedList()
+    {
+        Node<T> *tmp = NEWNODE;
+        tmp->next = NULL;
+        head = tmp;
+    }
+
+    ~LinkedList()
+    {
+        clear_elements();
+    }
+
+
+    void append(T value)
+    {
+        if ( !head->val ) {
+            head->val = value;
+        } else {
+            Node<T> *tmp = NEWNODE;
+            Node<T> *curr = head;
+            tmp->val = value;
             tmp->next = NULL;
+
+            while ( curr->next ) {
+                curr = curr->next;
+            }
+            curr->next = tmp;
+        }
+    }
+
+    void debug()
+    {
+        Node<T> *tmp = head;
+
+        printf("[");
+        while ( tmp ) {
+
+            if (tmp->next) {
+                std::cout << tmp->val << ", ";
+            } else if ( !tmp->val ){
+                continue;
+            } else {
+                std::cout << tmp->val;
+            }
+
+            tmp = tmp->next;
+        }
+        printf("]\n");
+    }
+
+    void push(T value)
+    {
+        if ( !head->val ) {
+            head->val = value;
+        } else {
+            Node<T> *tmp = NEWNODE;
+            tmp->val = value;
+            tmp->next = head;
             head = tmp;
         }
-
-        void append(T value)
-        {
-            if ( !head->val ) {
-                head->val = value;
-            } else {
-                Node<T> *tmp = (Node<T>*) malloc(sizeof(Node<T>));
-                Node<T> *curr = head;
-                tmp->val = value;
-                tmp->next = NULL;
-
-                while ( curr->next ) {
-                    curr = curr->next;
-                }
-                curr->next = tmp;
-            }
-        }
-
-        void debug()
-        {
-            Node<T> *tmp = head;
-
-            printf("[");
-            while ( tmp ) {
-
-                if (tmp->next) {
-                    std::cout << tmp->val << ", ";
-                } else if ( !tmp->val ){
-                    std::cout << "";
-                } else {
-                    std::cout << tmp->val;
-                }
-
-                tmp = tmp->next;
-            }
-            printf("]\n");
-        }
-
-        void push(T value)
-        {
-            if ( !head->val ) {
-                head->val = value;
-            } else {
-                Node<T> *tmp = (Node<T>*) malloc(sizeof(Node<T>));
-                tmp->val = value;
-                tmp->next = head;
-                head = tmp;
-            }
-        }
-
-        // Time Complexity : O(n)
-        void clear()
-        {
-            Node<T> *tmp;
-
-            while (head)
-            {
-                tmp = head;
-                head = head->next;
-                free(tmp);
-            }
-            head = NULL;
-        }
+    }
+    
+    void clear() {
+        clear_elements();
+        head = NULL;
+    }
 };
 
 int main()
